8bf4a6446a6a6db9a59b4fbd16b81676fb08d17a
Aaron Ward <Lord>
2021-08-19 07:34:26 -0400

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Aaron Ward on 2021-08-19.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
